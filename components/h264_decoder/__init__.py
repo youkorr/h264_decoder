122bcae0ed76051271edb8e696f8ae60b1305f0a
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-14 17:55:32 +0200

    Update __init__.py

--- components/h264_decoder/__init__.py
+++ components/h264_decoder/__init__.py
@@ -1,13 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import esp32
-from esphome.const import (
-    CONF_ID,
-    CONF_TRIGGER_ID,
-    DEVICE_CLASS_EMPTY,
-    ENTITY_CATEGORY_NONE,
-)
-from esphome.core import CORE, coroutine_with_priority
 from esphome import automation
 
 DEPENDENCIES = ["esp32"]
@@ -15,24 +8,17 @@ DEPENDENCIES = ["esp32"]
 h264_decoder_ns = cg.esphome_ns.namespace("h264_decoder")
 H264DecoderComponent = h264_decoder_ns.class_("H264DecoderComponent", cg.Component)
 
-# Struct DecodedFrame
 DecodedFrame = h264_decoder_ns.struct("DecodedFrame")
-
-# Actions
 DecodeFrameAction = h264_decoder_ns.class_("DecodeFrameAction", automation.Action)
-
-# Triggers
 FrameDecodedTrigger = h264_decoder_ns.class_("FrameDecodedTrigger", automation.Trigger.template(DecodedFrame.operator("ref")))
 DecodeErrorTrigger = h264_decoder_ns.class_("DecodeErrorTrigger", automation.Trigger.template(cg.std_string.operator("const").operator("ref")))
 
-# Enums
 PixelFormat = h264_decoder_ns.enum("PixelFormat")
 PIXEL_FORMATS = {
     "RGB565": PixelFormat.RGB565,
     "RGB888": PixelFormat.RGB888,
 }
 
-# Configuration keys
 CONF_H264_DECODER_ID = "h264_decoder_id"
 CONF_FRAME_BUFFER_SIZE = "frame_buffer_size"
 CONF_MAX_FRAME_WIDTH = "max_frame_width"
@@ -64,8 +50,7 @@ CONFIG_SCHEMA = cv.All(
                 }
             ),
         }
-    )
-    .extend(cv.COMPONENT_SCHEMA),
+    ).extend(cv.COMPONENT_SCHEMA),
     cv.only_on_esp32,
 )
 
@@ -73,18 +58,16 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    # Configuration des paramètres
     if CONF_FRAME_BUFFER_SIZE in config:
         cg.add(var.set_frame_buffer_size(config[CONF_FRAME_BUFFER_SIZE]))
     
     cg.add(var.set_max_frame_size(
-        config[CONF_MAX_FRAME_WIDTH], 
+        config[CONF_MAX_FRAME_WIDTH],
         config[CONF_MAX_FRAME_HEIGHT]
     ))
     
     cg.add(var.set_pixel_format(config[CONF_PIXEL_FORMAT]))
     
-    # Configuration des triggers
     for conf in config.get(CONF_ON_FRAME_DECODED, []):
         trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
         await automation.build_automation(trigger, [(DecodedFrame.operator("ref"), "frame")], conf)
@@ -93,7 +76,6 @@ async def to_code(config):
         trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
         await automation.build_automation(trigger, [(cg.const_char_ptr, "error")], conf)
 
-# Action pour décoder une frame
 @automation.register_action(
     "h264_decoder.decode_frame",
     DecodeFrameAction,
